1ffbf48a4f7d25a54daebc896a1b5c1a30a556a0
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-21 09:04:15 +0300

    copy

---
+++ CNA/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 250, green: 253, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 0, green: 180, blue: 233)
+    static let mainColorAlpha = UIColor(integralRed: 0, green: 180, blue: 233, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 247, green: 95, blue: 120)
+    
+    static let progress1Color = UIColor(integralRed: 0, green: 180, blue: 233)
+    static let progress2Color = UIColor(integralRed: 247, green: 95, blue: 120)
+    static let progress3Color = UIColor(integralRed: 247, green: 204, blue: 95)
+}
---
+++ CNA/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams.korrekted.com"
+    static let apiKey = "z4SnyY1pXN2rXRMq"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "J43cJqpaOowYeWz8"
+
+    static let applicationTag = "EXAM_CNA"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1568232839"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
